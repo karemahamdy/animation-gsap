e763ce933d9c6a55990142c216b2168454e732ee
Karema <karemahamdy51@gmail.com>
2024-07-09 02:13:15 +0300

    animation in photo , buttons, cards, lines

--- script.js
+++ script.js
@@ -102,3 +102,74 @@ gsap.utils.toArray('.line').forEach(line=>{
 
   })
 })
+
+// button
+gsap.utils.toArray('button').forEach(button=>{
+  gsap.fromTo(button,{
+      opacity:0,
+      x:150,
+      skewX:30
+  },{
+      opacity:1,
+      x:0,
+      skewX:0,
+      duration:1,
+      delay:.5,
+      scrollTrigger:button
+
+  })
+})
+
+
+// image
+gsap.utils.toArray('.rotation').forEach(rotate=>{
+  gsap.fromTo(rotate,{
+      opacity:0,
+      rotation:350,
+      scale:.2
+  },{
+      opacity:1,
+      rotation:0,
+      scale:1,
+      duration:1,
+      delay:1,
+      scrollTrigger:rotate
+
+  })
+})
+
+gsap.fromTo('.card' ,{
+  opacity:0,
+  scale:.1,
+},{
+  opacity:1,
+  scale:1,
+  duration:1,
+  delay:.5,
+  stagger:{
+      amount:1
+  },
+  scrollTrigger:'.card'
+})
+
+// hand and pyramid
+
+gsap.from('.pyramid' ,{
+  opacity:0,
+  scale:.5,
+  duration:1,
+  delay:.5
+})
+
+gsap.fromTo('.hand',{
+  scale:.2,
+  opacity:0,
+  skewY:30
+},{
+  scale:1,
+  opacity:1,
+  skewY:0,
+  duration:1,
+  delay:.5,
+  scrollTrigger:'.hand'
+})
